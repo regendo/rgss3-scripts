9a5cb69afdcd476706637f32e1cb36ff308609d0
regendo <regendo.kira@hotmail.com>
2011-12-18 20:17:51 +0100

    Added a replay battle method to GameOver_with_choices

--- GameOver_with_choices/gameover_with_choices.rb
+++ GameOver_with_choices/gameover_with_choices.rb
@@ -5,8 +5,11 @@
 #============================================
 # GameOver with choices
 #============================================
-# adds three choices to Scene_Gameover:
-# Load Savegame, Return to Title, Quit Game
+# adds four choices to Scene_Gameover:
+# Retry Battle (if you lost the battle)
+# Load Savegame (if there is a savegame)
+# Return to Title, Quit Game
+#============================================
 # implement over Main
 #============================================
 
@@ -28,6 +31,7 @@ class Window_GameOver < Window_Command
 	end
 	
 	def make_command_list
+    add_command("Retry Battle", :tryagain) if SceneManager.scene.is_defeat?
 		add_command("Load Savestate", :load, load_enabled)
 		add_command(Vocab::to_title, :to_title)
 		add_command(Vocab::shutdown, :shutdown)
@@ -56,6 +60,7 @@ class Scene_Gameover < Scene_Base
 	
 	def create_command_window
 		@command_window = Window_GameOver.new
+    @command_window.set_handler(:tryagain, method(:command_retry)) if is_defeat?
 		@command_window.set_handler(:load, method(:command_load))
 		@command_window.set_handler(:to_title, method(:goto_title))
 		@command_window.set_handler(:shutdown, method(:command_shutdown))
@@ -83,4 +88,67 @@ class Scene_Gameover < Scene_Base
 		fadeout_all
 		SceneManager.exit
 	end
+  
+  def command_retry
+    SceneManager.goto(Scene_Battle)
+    BattleManager.setup(@troop_id, @can_escape, @can_lose)
+    $game_party.members.each do |actor|
+      actor.recover_all
+    end
+    $game_troop.members.each do |enemy|
+      enemy.recover_all
+    end
+    BattleManager.bmgs_by_regendo(@map_bgm, @map_bgs)
+  end
+  
+  def is_defeat (b = true)
+    @defeat = b
+  end
+  
+  def is_defeat?
+    @defeat
+  end
+  
+  def battle_setup (troop_id, can_escape = true, can_lose = false)
+    @troop_id = troop_id
+    @can_escape = can_escape
+    @can_lose = can_lose
+  end
+  
+  def bgms_setup(map_bgm, map_bgs)
+    @map_bgm = map_bgm
+    @map_bgs = map_bgs
+  end
+end
+
+module BattleManager
+  class << self
+    alias_method :setup_old, :setup
+  end
+  def self.setup(troop_id, can_escape = true, can_lose = false)
+    self.setup_old(troop_id, can_escape = true, can_lose = false)
+    @troop_id = troop_id
+  end
+  
+  def self.bmgs_by_regendo(map_bgm, map_bgs)
+    @map_bgm = map_bgm
+    @map_bgs = map_bgs
+  end
+  
+  def self.process_defeat
+    $game_message.add(sprintf(Vocab::Defeat, $game_party.name))
+    wait_for_message
+    if @can_lose
+      revive_battle_members
+      replay_bgm_and_bgs
+      SceneManager.return
+    else
+      SceneManager.goto(Scene_Gameover)
+      SceneManager.scene.is_defeat #this is new
+      SceneManager.scene.battle_setup(@troop_id, @can_escape, @can_lose) #this also
+      SceneManager.scene.bgms_setup(@map_bgm, @map_bgs) #and this
+    end
+    battle_end(2)
+    return true
+  end
 end
