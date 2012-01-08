f3363e9684430ae0c8620eeb3a429d32aa0428e3
regendo <regendo.kira@hotmail.com>
2012-01-08 16:42:03 +0100

    [Menu_during_Message] added comments | improved customization

--- MenuScreen_while_message/menuscreen_while_message.rb
+++ MenuScreen_while_message/menuscreen_while_message.rb
@@ -1,4 +1,23 @@
-﻿module Regendo
+﻿#===================================
+# by bStefan aka. regendo
+# by request from AABattery
+# : http://www.rpgmakervxace.net/index.php?/user/608-aabattery/
+# please give credit if used
+# for use with RMVX ACE
+#===================================
+# Call Scene_Menu while a message
+# : is being displayed
+#===================================
+# implement over Main
+#===================================
+# customize:
+# : add Scenes you don't want the
+# : script to happen to NOCALLMENU
+# : (like Scene_Battle, which would
+# : be really annoying)
+#===================================
+
+module Regendo
   
   unless @scripts
     @scripts = Hash.new
@@ -14,15 +33,17 @@
     #CONFIG
     #=======
     NOCALLMENU = [Scene_Battle] #scenes in which call_menu shall not work.
+	BUTTON = Input::B #which button will trigger the menu?
   end
 end
   
 class Window_Message < Window_Base
+  BUTTON = Regendo::Menu_during_Message::BUTTON
   NOCALLMENU = Regendo::Menu_during_Message::NOCALLMENU
   alias update_old update
   def update
     update_old
-    call_menu if Input.trigger?(:B) && !forbidden_scene_by_regendo
+    call_menu if Input.trigger?(BUTTON) && !forbidden_scene_by_regendo
   end
   
   def call_menu
@@ -34,7 +55,14 @@ class Window_Message < Window_Base
   def input_pause
     self.pause = true
     wait(10)
-    Fiber.yield until Input.trigger?(:C)
+	case BUTTON
+	when Input::B
+	  Fiber.yield until Input.trigger?(:C)
+	when Input::C
+	  Fiber.yield until Input.trigger?(:B)
+	else
+      Fiber.yield until Input.trigger?(:B) || Input.trigger?(:C)
+	end
     Input.update
     self.pause = false
   end
