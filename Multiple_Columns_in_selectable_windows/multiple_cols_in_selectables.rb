7907f3ca1d6fe1429ebe2c3402d4d264928cdf6f
regendo <regendo.kira@hotmail.com>
2012-01-08 16:41:18 +0100

    [Multiple Cols] added comments

--- Multiple_Columns_in_selectable_windows/multiple_cols_in_selectables.rb
+++ Multiple_Columns_in_selectable_windows/multiple_cols_in_selectables.rb
@@ -9,6 +9,26 @@
 # C&P this as a new script above Main
 #==========================================
 
+#============================================================
+#                         HOW TO USE
+#============================================================
+# 1. make a new window class
+#   class Window_Something < Window_HorizontalCommand
+# 2. add commands to Window_Something#make_command_list
+#   add_command("Displayed_Name", :symbol, condition)
+# example:
+#   add_command("Give Item", :give, $game_variables[0] > 5)
+# 3. make a scene use this window
+#   @command_window = Window_Something.new
+#   @command_window.set_handler(:give, method(:give_item))
+# 4. define that method
+#   def give_item
+#     #code
+#   end
+# 5. ???
+# 6. Profit.
+#============================================================
+
 
 module Regendo
   
@@ -24,14 +44,18 @@ module Regendo
     #==============
     # CONFIG
     #==============
+	
+	#spacing in pixels
     def self.spacing
       32
     end
     
+	#padding in pixels
     def self.standard_padding
       12
     end
     
+	#number of columns if no number is given upon calling intialize	
     def self.standard_cols
       2
     end
@@ -69,29 +93,31 @@ class Window_HorizontalCommand < Window_Command
     (item_width + spacing) * col_max - spacing + standard_padding * 2
   end
   
-  def item_width
-    bmp = Bitmap.new(1,1)
-    length = []
-    @list.each do |index|
-      length.push((bmp.text_size(index[:name])).width)
-    end
-    if @width
-      if length.max > (@width - standard_padding * 2 + spacing) / col_max - spacing
-        (@width - standard_padding * 2 + spacing) / col_max - spacing
-      else
-        length.max
-      end
-    else
-      length.max
-    end
+ def item_width
+  bmp = Bitmap.new(1,1)
+  length = []
+  @list.each do |index|
+   length.push((bmp.text_size(index[:name])).width)
   end
+  if @width
+   if length.max > (@width - standard_padding * 2 + spacing) / col_max - spacing
+      (@width - standard_padding * 2 + spacing) / col_max - spacing
+   else
+      length.max
+   end
+  else
+   length.max
+  end
+ end
   
   def spacing
     Regendo::Horizontal_Command::spacing
   end
+  
   def standard_padding
     Regendo::Horizontal_Command::standard_padding
   end
+  
   def standard_cols
     Regendo::Horizontal_Command::standard_cols
   end
