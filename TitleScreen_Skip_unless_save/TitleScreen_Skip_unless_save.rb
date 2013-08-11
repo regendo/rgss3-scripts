2f118e5257c3da93e020ce2eab2145575a44a91c
regendo <bStefan100+git@gmail.com>
2013-08-11 17:22:52 +0200

    [Title Screen Skip]: minor changes
    replaced tags with double-space
    slightly changed method

--- TitleScreen_Skip_unless_save/TitleScreen_Skip_unless_save.rb
+++ TitleScreen_Skip_unless_save/TitleScreen_Skip_unless_save.rb
@@ -11,16 +11,14 @@
 #=====================================
 
 module SceneManager
-	class << self
-		alias :f_s_c_skips_title_screen :first_scene_class
-	end
-	def self.first_scene_class
-		if DataManager.save_file_exists?
-			f_s_c_skips_title_screen
-		else
-			DataManager.setup_new_game
-			$game_map.autoplay
-			Scene_Map
-		end
-	end
+  class << self
+    alias :f_s_c_skips_title_screen :first_scene_class
+  end
+  def self.first_scene_class
+    return f_s_c_skips_title_screen if DataManager.save_file_exists?
+    DataManager.setup_new_game
+    $game_map.autoplay
+    Scene_Map
+    end
+  end
 end
