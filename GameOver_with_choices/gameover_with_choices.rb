a2e120d2c0340dc44f3bf3faf0ee73b3199c7dec
regendo <bStefan100+git@gmail.com>
2013-08-12 14:39:25 +0200

    2.0-gowc pre
    Started rewrite of GameOver with Choices
    Does not yet support all functions of prior releases

--- GameOver_with_choices/gameover_with_choices.rb
+++ GameOver_with_choices/gameover_with_choices.rb
@@ -1,20 +1,34 @@
-﻿#=================================================
-# by bStefan aka. regendo
-# please give credit if used
-# for use with RMVX ACE
-#=================================================
-# GameOver with choices
-#=================================================
-# adds four choices to Scene_Gameover:
-# : Retry Battle (if you lost the battle)
-# : Load Savegame (if there is a savefile)
-# : Return to Title
-# : Quit Game
-#=================================================
-# implement over Main
-# implement under Multiple_Cols_in_Command_Window
-#  if existant
-#=================================================
+﻿#========================
+# GameOver with Choices
+#------------------------
+# for RMVXAce
+#========================
+# brought to you by
+# regendo, aka. bStefan
+#------------------------
+# up-to-date versions are exclusively available at
+# http://www.rpgmakervxace.net/topic/357-gameover-with-choices/
+# and https://github.com/regendo/rgss3-scripts
+#========================
+
+# The Terms of Use specified in the README.md file in the github repository apply to this script.
+
+#========================
+# CHANGELOG
+#========================
+# current release: 2.0-gowc pre
+# current release notes:
+# # complete rewrite
+# # not all functionalities already supported
+#------------------------
+# 2.0   - complete rewrite
+# 1.3   - now able to regain items, weapons, armours when retrying battles
+# 1.2   - now compatible with "Multiple Columns in selectable windows" script by regendo
+# 1.1   - now able to retry lost battles
+# 1.0   - initial release
+
+# above update notes do not include minor updates such as bug fixes
+#========================
 
 module Regendo
   
@@ -27,234 +41,153 @@ module Regendo
   @scripts["GameOver_Window"] = true
   
   module GameOver_Window
-    def self.multiple_cols?
-      return false unless Regendo.contains?("Horizontal_Command")
-      USE_MULTIPLE_COLUMNS
-    end
-    #=======
-    #CONFIG
-    #=======
-	
-    RETRY = true #if false, Retry Battle function will not be aviable
   
-    REGAIN = false # if true, Retry Battle will make the party regain any
-                   #  items, weapons, or armours lost in that battle.
+    # CUSTOMISATION OPTIONS
     
-    #==============================================
-    #requires Horizontal_Command script by regendo
-    #==============================================
-    if Regendo.contains?("Horizontal_Command")
-       USE_MULTIPLE_COLUMNS = true #use Horizontal_Command Script?
-       COLUMNS = 2 #requires  ^ set to true
-    end
-     
-    #=================================================
-    #only used if not using Horizontal_Command Script
-    #=================================================
-      WINDOW_WIDTH = 225
+    RETRY_TEXT = "Retry Battle"
+    
+    LOAD_TEXT = "Load Savefile"
+  
+    # allows a lost battle to be retried
+    # retry only works when "continue even if lose" is not checked
+    # set by script call: Regendo::GameOver_Window::ALLOW_RETRY = true/false
+    ALLOW_RETRY = false
+    
+    # allows loading a save file
+    # set by script call: Regendo::GameOver_Window::ALLOW_LOAD_GAME = true/false
+    ALLOW_LOAD_GAME = true
+    
+    
+    # use a window with multiple columns
+    # requires Multiple Cols in Command Windows script by regendo
+    USE_MULTIPLE_COLS = false
+    COL_NUMBER = 2
+    
+    # X and Y coordinates for the window as a String
+    # default X: "[(Graphics.width - width)/2, 0].max"
+    # default Y: "[(Graphics.height - height)/1.1, 0].max"
+    X_COORD = "[(Graphics.width - width)/2, 0].max"
+    Y_COORD = "[(Graphics.height - height)/1.1, 0].max"
+    
   end
 end
 
-if Regendo::GameOver_Window.multiple_cols?
-  class Window_GameOver < Window_HorizontalCommand #more than one column possible
+# DO NOT EDIT BELOW
+
+class Scene_Gameover < Scene_Base
+  
+  alias :start_with_regendo_gameover_window :start
+  def start
+    start_with_regendo_gameover_window
+    create_command_window
+  end
+  
+  # overwrites update method
+  # prevents returning to title screen upon button press
+  def update
+    super
+  end
+  
+  def pre_terminate
+    super
+    close_command_window
+    fadeout_all
+  end
+  
+  def create_command_window
+    @command_window = Window_GameOver.new
+    set_handlers
+  end
+  
+  def set_handlers
+    @command_window.set_handler(:retry, method(:command_retry))
+    @command_window.set_handler(:load, method(:command_load_game))
+    @command_window.set_handler(:to_title, method(:goto_title))
+    @command_window.set_handler(:shutdown, method(:command_shutdown))
+  end
+  
+  def close_command_window
+    @command_window.close if @command_window
+    update until @command_window.close?
+  end
+  
+  def command_retry
+    # TODO
+  end
+  
+  def command_load_game
+    SceneManager.goto(Scene_Load)
+  end
+  
+  def command_shutdown
+    SceneManager.exit
+  end
+  
+end
+
+if (Regendo.contains?("Horizontal_Command") && Regendo::GameOver_Window::USE_MULTIPLE_COLS)
+  class Window_GameOver < Window_HorizontalCommand
   end
 else
-  class Window_GameOver < Window_Command #only one column
+  class Window_GameOver < Window_Command
   end
 end
 
 class Window_GameOver
-	def initialize
-		if Regendo::GameOver_Window.multiple_cols?
-          if Regendo::GameOver_Window::COLUMNS
-		    super(0, 0, Regendo::GameOver_Window::COLUMNS)
-		  else
-		    super(0, 0)
-		  end
-        else
-          super(0, 0)
-        end
-		update_placement
-		self.openness = 0
-		open
-	end
-	
-  unless Regendo::GameOver_Window.multiple_cols?
-    def window_width
-        Regendo::GameOver_Window::WINDOW_WIDTH
-    end
-  end
-  
-	def update_placement
-		self.x = (Graphics.width - width) / 2
-		self.y = (Graphics.height - height) / 1.1
-	end
-	
-	#======================================
-	# add your own to this list
-	# also requires changes at
-	# Scene_Gameover#create_command_window
-	#======================================
-	def make_command_list
-        add_command("Retry Battle", :tryagain) if SceneManager.scene.is_defeat?
-		add_command("Load Savestate", :load, load_enabled)
-		add_command(Vocab::to_title, :to_title)
-		add_command(Vocab::shutdown, :shutdown)
-	end
-	
-	def load_enabled
-		DataManager.save_file_exists?
-	end
-end
 
-class Scene_Gameover < Scene_Base
-    attr_reader :command_window
-	alias start_old start
-	def start
-		start_old
-		create_command_window
-	end
-	
-	def pre_terminate
-		super
-		close_command_window
-	end
-	
-	def update
-		super
-	end
-	
-	#======================================
-	# add your own to this list
-	# also requires changes at
-	# Window_GameOver#make_command_list
-	# and requires adding your own methods
-	#======================================
-	
-	def create_command_window
-		@command_window = Window_GameOver.new
-        @command_window.set_handler(:tryagain, method(:command_retry)) if is_defeat?
-		@command_window.set_handler(:load, method(:command_load))
-		@command_window.set_handler(:to_title, method(:goto_title))
-		@command_window.set_handler(:shutdown, method(:command_shutdown))
-	end
-	
-	def close_command_window
-		@command_window.close if @command_window
-		update until @command_window.close?
-	end
-	
-	def command_load
-		close_command_window
-		fadeout_all
-		SceneManager.call(Scene_Load)
-	end
-	
-	def goto_title
-		close_command_window
-		fadeout_all
-		SceneManager.goto(Scene_Title)
-	end
-	
-	def command_shutdown
-		close_command_window
-		fadeout_all
-		SceneManager.exit
-	end
-  
-  def command_retry
-	  close_command_window
-    fadeout_all
-    regain_stuff if regain_stuff?
-    SceneManager.goto(Scene_Battle)
-    BattleManager.setup(@troop_id, @can_escape, @can_lose)
-    $game_party.members.each do |actor|
-      actor.recover_all
-    end
-    BattleManager.bmgs_by_regendo(@map_bgm, @map_bgs)
-    BattleManager.play_battle_bgm
-    Sound.play_battle_start
-  end
-  
-  def is_defeat (b = true)
-    @defeat = b
-  end
-  
-  def is_defeat?
-    Regendo::GameOver_Window::RETRY ? @defeat : false
-  end
-  
-  def regain_stuff
-    $game_party.regendo_set_items(@items)
-    $game_party.regendo_set_weapons(@weapons)
-    $game_party.regendo_set_armours(@armours)
-  end
-  
-  def regain_stuff?
-    Regendo::GameOver_Window::RETRY ? Regendo::GameOver_Window::REGAIN : false
-  end
-  
-  def battle_setup (regendo_new_values, can_escape = true, can_lose = false)
-    @troop_id = regendo_new_values[:troop_id]
-    @items = regendo_new_values[:items]
-    @weapons = regendo_new_values[:weapons]
-    @armours = regendo_new_values[:armours]
-    @can_escape = can_escape
-    @can_lose = can_lose
-  end
-  
-  def bgms_setup(map_bgm, map_bgs)
-    @map_bgm = map_bgm
-    @map_bgs = map_bgs
-  end
-end
-
-module BattleManager
-  class << self
-    alias_method :setup_old, :setup
-  end
-  def self.setup(troop_id, can_escape = true, can_lose = false)
-    self.setup_old(troop_id, can_escape, can_lose)
-    @regendo_new_values = {}
-    @regendo_new_values[:troop_id] = troop_id
-    @regendo_new_values[:items] = $game_party.regendo_copy_items
-    @regendo_new_values[:weapons] = $game_party.regendo_copy_weapons
-    @regendo_new_values[:armours] = $game_party.regendo_copy_armours
-  end
-  
-  def self.bmgs_by_regendo(map_bgm, map_bgs)
-    @map_bgm = map_bgm
-    @map_bgs = map_bgs
-  end
-  
-  def self.process_defeat
-    $game_message.add(sprintf(Vocab::Defeat, $game_party.name))
-    wait_for_message
-    if @can_lose
-      revive_battle_members
-      replay_bgm_and_bgs
-      SceneManager.return
+  def initialize
+    if (Regendo.contains?("Horizontal_Command") && Regendo::GameOver_Window::USE_MULTIPLE_COLS)
+      super(0, 0, Regendo::GameOver_Window::COL_NUMBER)
+      @horz = true
     else
-      SceneManager.goto(Scene_Gameover)
-      SceneManager.scene.is_defeat
-      SceneManager.scene.battle_setup(@regendo_new_values, @can_escape, @can_lose) #this also
-      SceneManager.scene.bgms_setup(@map_bgm, @map_bgs)
+      super(0, 0)
+      @horz = false
     end
-    battle_end(2)
-    return true
+    update_placement
+    self.openness = 0
+    open
   end
+  
+  def window_width
+    return super if @horz
+    return Graphics.width * 0.4
+  end
+  
+  def update_placement
+    self.x = eval(Regendo::GameOver_Window::X_COORD)
+    self.y = eval(Regendo::GameOver_Window::Y_COORD)
+  end
+  
+  def make_command_list
+    add_command(retry_text, :retry) if Regendo::GameOver_Window::ALLOW_RETRY
+    add_command(load_text, :load, can_load_game?) if Regendo::GameOver_Window::ALLOW_LOAD_GAME
+    add_command(Vocab.to_title, :to_title)
+    add_command(Vocab.shutdown, :shutdown)
+  end
+  
+  def retry_text; Regendo::GameOver_Window::RETRY_TEXT; end
+  def load_text; Regendo::GameOver_Window::LOAD_TEXT; end
+  
+  def can_load_game?
+    DataManager.save_file_exists?
+  end
+  
 end
 
 class Game_Party < Game_Unit
   
-  def regendo_copy_items; Marshal.load(Marshal.dump(@items)); end
-  def regendo_copy_weapons; Marshal.load(Marshal.dump(@weapons)); end
-  def regendo_copy_armours; Marshal.load(Marshal.dump(@armors)); end
-  def regendo_copy_armors; Marshal.load(Marshal.dump(@armors)); end
-    
-  def regendo_set_items(new_items); @items = new_items; end
-  def regendo_set_weapons(new_weapons); @weapons = new_weapons; end
-  def regendo_set_armours(new_armours); @armors = new_armours; end
-  def regendo_set_armors(new_armors); @armors = new_armors; end
+end
+
+module BattleManager
+  
+  class << self
+    alias_method :setup_old, :setup
+  end
+  
+  def self.setup(troop_id, can_escape = true, can_lose = false)
+    setup_old(troop_id, can_escape, can_lose)
+    @regendo_gameover_values = Hash.new
+    @regendo_gameover_values[:troop_id] = troop_id
+  end
   
 end
